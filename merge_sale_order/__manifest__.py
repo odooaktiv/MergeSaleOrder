511f583dfe0a28836eb25771957de2825fe36f9f
Virali Aktiv <mgajjar@nishutatechnolabs.com>
2018-05-29 18:11:15 +0530

    [INIT] Initial commit for module merge sale order in version 10.0

--- merge_sale_order/__manifest__.py
+++ merge_sale_order/__manifest__.py
@@ -16,11 +16,7 @@
     'data': [
         'wizard/merge_sale_order_wizard_view.xml',
     ],
-
-    'images': [
-        'static/description/banner.jpg',
-    ],
-
+    'images':  ['static/description/banner.jpg'],
     'auto_install': False,
     'installable': True,
     'application': False
