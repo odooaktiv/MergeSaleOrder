52d034ecfc26c61f1110240ac39ae4e4cf3893ea
Odoo Aktiv <solankiriddhir@yahoo.in>
2018-09-07 17:49:05 +0530

    [IMP] license in manifest for branch 10.0

--- merge_sale_order/__manifest__.py
+++ merge_sale_order/__manifest__.py
@@ -6,6 +6,7 @@
     'version': '1.1',
     'website': 'http://www.aktivsoftware.com',
     'author': 'Aktiv Software',
+    'license': 'AGPL-3',
     'description': 'Merge Sale Order',
 
     'depends': [
